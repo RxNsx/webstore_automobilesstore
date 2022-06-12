731120f1991d283e0a501f483a7e4fb3be1df46f
rxnsx <alpendev@mail.ru>
2022-06-12 11:46:51 +0700

    First Commit

---
+++ WebStoreAutomobiles/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WebStoreAutomobiles.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
